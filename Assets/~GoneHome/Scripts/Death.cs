bd6d66796ae9040e77914355005103466d51595a
ty.wilson7 <ty.wilson7@600900B211DE413>
2018-02-22 11:51:46 +1100

    Created GoneHome game

---
+++ Assets/~GoneHome/Scripts/Death.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GoneHome
+{
+    public class Death : MonoBehaviour
+    {
+
+        void Died()
+        {
+            //Play animation
+            //spawn particles
+            //preform other event
+
+
+            print("The " + name + " has died");
+        }
+
+        //Detect collision with other triggers
+        private void OnTriggerEnter(Collider other)
+        {
+            //have we hit a "DeathZone" or "Enemy"
+            if(other.name == "DeathZone" || other.name == "Enemy")
+            {
+                //KILL IT
+                Died();
+            }
+
+            // if (other.name == "Enemy")
+            // {
+            //KILL IT
+            //    Died();
+            // }
+
+
+            if (other.name == "player")
+            {
+                audio.Play();
+            }
+
+        }
+
+    }
+}
---
+++ Assets/~GoneHome/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Clean code: CTRL + K + D
+// Fold code: CTRL + M + P
+// Un-Fold code: CTRL + M + O
+
+namespace GoneHome
+{
+    public class Player : MonoBehaviour
+    {
+        public float movementSpeed = 20f;
+
+        private Rigidbody rigid;
+        private Transform cam;
+
+        // Use this for initialization
+        void Start()
+        {
+            rigid = GetComponent<Rigidbody>();
+
+            cam = Camera.main.transform;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Movement();
+        }
+
+        void Movement()
+        {
+            float inputH = Input.GetAxis("Horizontal");
+            float inputV = Input.GetAxis("Vertical");
+
+            Vector3 inputDir = new Vector3(inputH, 0, inputV);
+
+            Transform cam = Camera.main.transform;
+            inputDir = Quaternion.AngleAxis(cam.eulerAngles.y, Vector3.up) * inputDir;
+
+            // Moves the position of the rigidbody without 
+            // effecting gravity
+            Vector3 position = transform.position;
+            position += inputDir * movementSpeed * Time.deltaTime;
+            rigid.MovePosition(position);
+        }
+    }
+}
