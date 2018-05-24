abd42a5c52c983986a968d25a85504d026dc6a5e
ty.wilson7 <ty.wilson7@600900B211DE406>
2018-05-24 12:01:58 +1000

    Added Droid and AIAgent scripts

---
+++ Unity/Assets/~TowerDefense/Scripts/Enemies/AIAgent.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.AI;
+
+namespace TowerDefense
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class AIAgent : MonoBehaviour
+    {
+        public Transform target;
+
+        private NavMeshAgent nav;
+
+        void Awake()
+        {
+            //reference the mesh component
+            nav = GetComponent<NavMeshAgent>();
+        }
+
+        void Update()
+        {
+            //if a target has been set
+            if (target != null)
+            {
+                //navigate to the target
+                nav.SetDestination(target.position);
+            }
+        }
+    }
+}
---
+++ Unity/Assets/~TowerDefense/Scripts/Enemies/Droid.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TowerDefense
+{
+    public class Droid : MonoBehaviour
+    {
+        public float health = 100;
+        /// <summary>
+        /// Deals damage to the Droid
+        /// </summary>
+        /// <param name="damage">Damage to deal</param>
+        public void DealDamage(float damage)
+        {
+            // Deal damage to droid
+            health -= damage;
+            // If there is no health
+            if (health <= 0)
+            {
+                // Kill off GameObject
+                Destroy(gameObject);
+            }
+        }
+    }
+}
